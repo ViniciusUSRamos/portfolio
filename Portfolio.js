68d9a4b7cee0ab8519b5e8ce40d11c5e91dd9234
Vinícius Ramos <73314551+ViniciusUSRamos@users.noreply.github.com>
2025-09-18 00:14:08 -0300

    carrossel projetos

--- Portfolio.js
+++ Portfolio.js
@@ -1,18 +1,18 @@
 // abrir popup com a tag dialog
-const dialog = document.getElementById("conteudoPopup")
-const wrapper = document.querySelector(".wrapper")
+// const dialog = document.getElementById("conteudoPopup")
+// const wrapper = document.querySelector(".wrapper")
 
-function showDialog(){ //mostrar popup
-    dialog.showModal()
-}
-function closeDialog(){ 
-    dialog.close() // fecha o popup
-}
-dialog.addEventListener("click", (e) => { // fecha se clicar fora da caixa de dialogo
-    if(!wrapper.contains(e.target)){
-        dialog.close()
-    }
-});
+// function showDialog(){ //mostrar popup
+//     dialog.showModal()
+// }
+// function closeDialog(){ 
+//     dialog.close() // fecha o popup
+// }
+// dialog.addEventListener("click", (e) => { // fecha se clicar fora da caixa de dialogo
+//     if(!wrapper.contains(e.target)){
+//         dialog.close()
+//     }
+// });
 
 // ---------------
 // tornar o formulario funcional
@@ -34,19 +34,24 @@ botao.addEventListener("submit", function(event){
 
 document.addEventListener('DOMContentLoaded', () =>{
     const switchEl = document.getElementById('languageSwitch');
+    const lang = localStorage.getItem('lang');
+    if (!switchEl) return;
 
-    if (window.location.pathname.includes('indexEnglish.html')){
-        switchEl.checked = true;
-    }else{
-        switchEl.checked = false;
-    }
+
+    if (window.location.href.includes('indexEnglish.html')) {
+    switchEl.checked = true;
+  } else {
+    switchEl.checked = false;
+  }
 
     switchEl.addEventListener('change', () => {
     if (switchEl.checked) {
+        console.log('muda pra ingles')
         // Redireciona para inglês
         localStorage.setItem('lang', 'en');
         window.location.href = 'indexEnglish.html';
     } else {
+        console.log('muda pra pt')
         // Redireciona para português
         localStorage.setItem('lang', 'pt');
         window.location.href = 'index.html';
